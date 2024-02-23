5d3afa3faa5a3c887b8126071a881e63ccd85951
killmonger68 <milindtarale12@gmail.com>
2024-02-23 13:13:51 +0530

    first commit

---
+++ shoe/src/App.jsx
@@ -0,0 +1,16 @@
+// import Nav from "./components/Nav"
+import './App.css'
+import Hero from './components/Hero'
+import Nav from './components/Nav'
+
+const App = ()=>{
+  return(
+    <>
+  <div>
+<Nav></Nav>
+<Hero></Hero>
+  </div>
+    </>
+  )
+}
+export default App
---
+++ shoe/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import './nav.css'
+
+const Nav = () => {
+  return (
+    <div>
+        <nav>
+            <div className="logo">
+<img src="../../public/images/brand_logo.png" alt="" />
+            </div>
+<ul>
+ <li href="#">MENU</li>
+ <li href="#">LOCATION</li>
+ <li href="#">ABOUT</li>
+ <li href="#">CONTACT</li>
+</ul>
+
+<button>login</button>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
